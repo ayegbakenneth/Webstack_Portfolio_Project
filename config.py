2817efc3ea015ff1621a2fcea71c0f60c3e3433e
ayegbakenneth <117679811+ayegbakenneth@users.noreply.github.com>
2024-09-08 17:26:14 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ShopNice_App:CoutPrint@localhost:3306/my_new_database'
+    SQLALCHEMY_DATABASE_URI = 
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODiFICATIONs = False 
